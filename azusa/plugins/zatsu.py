5d46f91812ad907a868cc071bf4fdae9b22142e9
kachitoritai <Huqihao106425>
2022-05-02 01:17:08 +0800

    fix

--- azusa/plugins/zatsu.py
+++ azusa/plugins/zatsu.py
@@ -7,9 +7,9 @@ help = on_keyword(['/help'],priority = 5)
 @help.handle()
 async def help_handle():
     help_msg = "/help 【帮助信息】 \n"
-    +"来点二次元 或者 ldecy 【随机二次元图片】 \n"
-    "/echo 【复读（需@机器人）】"
-    await help.send(help_msg)
+    help_msg +="来点二次元 或者 ldecy 【随机二次元图片】 \n"
+    help_msg +="/echo 【复读（需@bot）】"
+    await help.finish(help_msg)
 
 #随机二次元图片
 some_nijigenn = on_keyword(['ldecy','来点二次元'],priority = 20)
@@ -18,4 +18,4 @@ async def some_nijigenn_handle():
     msg1 = '二次元来辣~'
     msg2 = MessageSegment.image('https://www.dmoe.cc/random.php')
     msg = msg1 + msg2
-    await some_nijigenn.send(msg)
+    await some_nijigenn.finish(msg)
